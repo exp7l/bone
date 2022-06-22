f448d4b542072b49d22cbeb0a73f32c10d51e42b
exp7l <107167107+exp7l@users.noreply.github.com>
2022-06-22 01:40:05 -0700

    testnet

--- docs/index.js
+++ docs/index.js
@@ -8,7 +8,8 @@ async function getConf() {
       fetch('DAI.json'),
       fetch('BMW.json'),
       fetch('BXAU.json'),
-      fetch('address-book-1337.json')
+      fetch('address-book-4.json')
+      // fetch('address-book-1337.json')
     ]
   )
   const [
@@ -16,9 +17,7 @@ async function getConf() {
     bmwABI,
     bxauABI,
     addressBook
-  ] = await Promise.all(
-    responses.map(resp => resp.json())
-  )
+  ] = await Promise.all(responses.map(resp => resp.json()))
   return { daiABI, bmwABI, bxauABI, addressBook }
 }
 
@@ -28,16 +27,6 @@ async function getConn() {
   const signer        = provider.getSigner()
   const signerAddress = await signer.getAddress(0)
   return { signer, signerAddress, network, }
-}
-
-function readable(bn) {
-  const quotient  = bn.div(ethers.constants.WeiPerEther).toString()
-  const remainder = bn.mod(ethers.constants.WeiPerEther).toString()
-  return `${quotient}.${remainder}`
-}
-
-function writeable(real) {
-  return ethers.BigNumber.from(real).mul(ethers.constants.WeiPerEther)  ;
 }
 
 function contracts({
@@ -55,16 +44,80 @@ function contracts({
 }
 
 async function assetData(contract, dai, signerAddress) {
+  const [
+    balance,
+    totalSupply,
+    mintPrice,
+    redemptionPrice,
+    liability,
+    cratioFloor,
+    allowance,
+    fee
+  ] = (await Promise.all([
+    contract.balanceOf(signerAddress),
+    contract.totalSupply(),
+    contract.mintPrice(),
+    contract.redemptionPrice(),
+    contract.liability(),
+    contract.cratioFloor(),
+    dai.allowance(signerAddress, contract.address),
+    contract.fee()
+  ])).map(x => ethers.utils.formatUnits(x))
   return {
-    "Your Balance                 ": readable(await contract.balanceOf(signerAddress)),
-    "Total Supply                 ": readable(await contract.totalSupply()),
-    "Mint Price              (DAI)": readable(await contract.mintPrice()),
-    "Redemption Price        (DAI)": readable(await contract.redemptionPrice()),
-    "Liability               (DAI)": readable(await contract.liability()),
-    "Mint C-Ratio Floor (multiple)": readable(await contract.cratioFloor()),
-    "DAI Approved            (DAI)": readable(await dai.allowance(signerAddress, contract.address)),
-    "Fee                       (%)": readable(await contract.fee()),
+    "Your Balance                 ": balance,
+    "Total Supply                 ": totalSupply,
+    "Mint Price              (DAI)": mintPrice,
+    "Redemption Price        (DAI)": redemptionPrice,
+    "Liability               (DAI)": liability,
+    "Mint C-Ratio Floor (multiple)": cratioFloor,
+    "DAI Approved            (DAI)": allowance,
+    "Fee                       (%)": fee,
     "address                      ": contract.address
+  }
+}
+
+async function reserveData(contract, signerAddress, daiContract) {
+  const [
+    balance,
+    mintPrice,
+    redemptionPrice,
+    cratioFloor,
+    cratio,
+    reserve,
+    liability,
+    equity,
+    totalSupply,
+    allowance,
+    fee
+  ] = await Promise.all([
+    contract.balanceOf(signerAddress),
+    contract.mintPrice(),
+    contract.redemptionPrice(),
+    contract.cratioFloor(),
+    contract.cratio(),
+    contract.reserve(),
+    contract.liability(),
+    contract.equity(),
+    contract.totalSupply(),
+    daiContract.allowance(signerAddress, contract.address),    
+    contract.fee(),
+  ])
+  return {
+    "Your Balance                       ": ethers.utils.formatUnits(balance),
+    "Mint Price                    (DAI)": ethers.utils.formatUnits(mintPrice),
+    "Redemption Price              (DAI)": ethers.utils.formatUnits(redemptionPrice),
+    "Redemption C-Ratio floor (multiple)": ethers.utils.formatUnits(cratioFloor),
+    "Global C-Ratio           (multiple)": ethers.utils.formatUnits(cratio),    
+    "Global Reserve                (DAI)": ethers.utils.formatUnits(reserve),
+    "Global Liablity               (DAI)": ethers.utils.formatUnits(liability),
+    "Global Equity                 (DAI)": ethers.utils.formatUnits(equity),
+    "Global Retailed Earning       (DAI)": ethers.utils.formatUnits(equity.sub(totalSupply)),
+    "Fee                             (%)": ethers.utils.formatUnits(fee),
+    "BMW Supply                         ": ethers.utils.formatUnits(totalSupply),
+    "address                            ": contract.address,
+    "DAI Approved                  (DAI)": ethers.utils.formatUnits(allowance),
+    // FIXME: make sure asset is deployed.
+    "Approved Assets                    ": [ await contract.synths(0) ],    
   }
 }
 
@@ -85,7 +138,7 @@ globalThis.onload = async function onLoad() {
     ...state.provider.connection,
     pollingInterval: state.provider._pollingInterval,
     signerAddress:   state.signerAddress,
-    daiBalace:       readable(await state.DAI.balanceOf(state.signerAddress))
+    daiBalace:       ethers.utils.formatUnits(await state.DAI.balanceOf(state.signerAddress))
   }, null, '\t')
   
   const renderAsset = async () => d.querySelector("pre#asset").textContent = JSON.stringify(
@@ -96,30 +149,15 @@ globalThis.onload = async function onLoad() {
     ), null, '\t')
   
   await renderAsset()
-  console.log(state)
-  d.querySelector("pre#reserve").textContent = JSON.stringify({
-    "Your Balance                       ": readable(await state.BMW.balanceOf(state.signerAddress)),
-    "Mint Price                    (DAI)": readable(await state.BMW.mintPrice()),
-    "Redemption Price              (DAI)": readable(await state.BMW.redemptionPrice()),
-    "Redemption C-Ratio floor (multiple)": readable(await state.BMW.cratioFloor()),
-    "Global C-Ratio           (multiple)": readable(await state.BMW.cratio()),    
-    "Global Reserve                (DAI)": readable(await state.BMW.reserve()),
-    "Global Liablity               (DAI)": readable(await state.BMW.liability()),
-    "Global Equity                 (DAI)": readable(await state.BMW.equity()),
-    "Global Retailed Earning       (DAI)": readable((await state.BMW.equity()).sub(await state.BMW.totalSupply())),
-    "Fee                             (%)": readable(await state.BMW.fee()),
-    "BMW Supply                         ": readable(await state.BMW.totalSupply()),
-    "address                            ": state.BMW.address,
-    // FIXME: make sure asset is deployed.
-    "Approved Assets                    ": [ await state.BMW.synths(0) ],    
-  }, null, '\t')
-
+  
   d.querySelector("select#picker").onchange = async () => await renderAsset()
+
+  d.querySelector("pre#reserve").textContent = JSON.stringify(await reserveData(state.BMW, state.signerAddress, state.DAI), null, '\t')
 
   d.querySelector("button#mint").onclick = async () => {
     const asset    = d.querySelector("select#picker").value    
     const quantity = d.querySelector("input#quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response  = await state[asset].mint(wad)
     console.log(await response.wait())
   }
@@ -127,36 +165,36 @@ globalThis.onload = async function onLoad() {
   d.querySelector("button#redeem").onclick = async () => {
     const asset    = d.querySelector("select#picker").value
     const quantity = d.querySelector("input#quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response  = await state[asset].redeem(wad)
     console.log(await response.wait())    
   }
 
-  d.querySelector("button#approve").onclick = async () => {
+   d.querySelector("button#approve").onclick = async () => {
     const asset    = d.querySelector("select#picker").value
     const quantity = d.querySelector("input#quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response = await state.DAI.approve(state.addressBook[asset], wad)
     console.log(await response.wait())
   }
 
   d.querySelector("button#r-mint").onclick = async () => {
     const quantity = d.querySelector("input#r-quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response  = await state.BMW.mint(wad)
     console.log(await response.wait())
   }
 
   d.querySelector("button#r-redeem").onclick = async () => {
     const quantity = d.querySelector("input#r-quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response  = await state.BMW.redeem(wad)
     console.log(await response.wait())
   }
 
   d.querySelector("button#r-approve").onclick = async () => {
     const quantity = d.querySelector("input#r-quantity").value
-    const wad      = writeable(quantity)
+    const wad      = ethers.utils.parseUnits(quantity)
     const response = await state.DAI.approve(state.addressBook['BMW'], wad)
     console.log(await response.wait())
   }
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,9 +8,14 @@ contract S is Script {
         external
     {
         // chainId: 1
-	address dai    = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-	address xauusd = 0x214eD9Da11D2fbe465a6fc601a91E62EbEc1a0D6;
-	address daiusd = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+	// address dai    = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+	// address xauusd = 0x214eD9Da11D2fbe465a6fc601a91E62EbEc1a0D6;
+	// address daiusd = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+
+        // chainId: 4
+	address dai       = 0x6A9865aDE2B6207dAAC49f8bCba9705dEB0B0e6D;
+	address xauusd    = 0x81570059A0cb83888f1459Ec66Aad1Ac16730243;
+	address daiusd    = 0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF;
         vm.startBroadcast();
 	BMW  bmw       = new BMW(dai);
 	BXAU bxau      = new BXAU(bmw, xauusd, daiusd, dai);
